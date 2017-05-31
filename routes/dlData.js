65b82df02141f284aa7ebc0fe9aa78749589413d
kevinalex1982 <1970553557@qq.com>
2017-05-31 23:07:48 +0800

    add findgames
    add findgames

--- routes/dlData.js
+++ routes/dlData.js
@@ -5,28 +5,24 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models/dbapi');
 
-/* GET users listing. */
+
 router.post('/getUsers', function (req, res, next) {
-    console.log(req.body);
-    console.log(req.body.start);
-    console.log(req.body.length);
+
 
 // 查询所有用户
 // 1：页号，从1开始，2：每页记录条数，3：role，0所有，1管理员，2团长，3团员
 // 4：回调，err如果成功为null，否则表示失败。属性error表示错误描述。users对应的用户信息
-    db.users.all_users(Number(req.body.start / req.body.length) + 1, req.body.length, req.body.param.role, (err, users) => {
+    db.users.all_users(Number(Number(req.body.start / req.body.length).toFixed(0))+1, req.body.length, req.body.param.role, (err, users) => {
         if (err) {
-            console.log(err.error);
-            return;
+            res.send('error');
+        } else {
+            var jsonSend = {
+                data: users.users, "iTotalDisplayRecords": users.total,
+                "iTotalRecords": users.total,
+                "message": "success"
+            };
+            res.send(jsonSend);
         }
-        ;
-        console.log(users);
-        var jsonSend = {
-            data: users.users, "iTotalDisplayRecords": users.total,
-            "iTotalRecords": users.total,
-            "message": "success"
-        };
-        res.send(jsonSend);
     });
 });
 
@@ -64,7 +60,6 @@ router.post('/addCaptain', function (req, res, next) {
     });
 })
 
-
 router.post('/addMember', function (req, res, next) {
 
 // 添加团员
@@ -79,7 +74,6 @@ router.post('/addMember', function (req, res, next) {
         }
     });
 })
-
 
 router.post('/updUser', function (req, res, next) {
     console.log(req.body.usloginpsw == "" ? null : req.body.usloginpsw);
@@ -113,4 +107,29 @@ router.post('/delUser', function (req, res, next) {
         }
     });
 })
+
+
+router.post('/getGames', function (req, res, next) {
+
+   /* console.log(Number(Number(req.body.start / req.body.length).toFixed(0))+1);
+    console.log(Number(req.body.length));
+    console.log(Number(req.body.usID));*/
+
+    // 查询所有游戏 userid=0 返回所有
+    db.games.all_games(Number(Number(req.body.start / req.body.length).toFixed(0))+1, Number(req.body.length),  Number(req.body.usID), (err, games) => {
+        if (err) {
+            res.send('error');
+        } else {
+            console.log(games);
+            var jsonSend = {
+                data: games.games, "iTotalDisplayRecords": games.total,
+                "iTotalRecords": games.total,
+                "message": "success"
+            };
+            res.send(jsonSend);
+        }
+    })
+
+});
+
 module.exports = router;
