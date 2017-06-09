34a85321e2d726fcadf88e904065a948ed012435
kevinalex1982 <1970553557@qq.com>
2017-06-09 22:56:32 +0800

    change games

--- routes/dlData.js
+++ routes/dlData.js
@@ -37,7 +37,7 @@ router.post('/getUsers', function (req, res, next) {
 
 router.post('/addAdmin', function (req, res, next) {
 
-    var curuser =req.body.curuser;
+    var curuser = req.body.curuser;
 // 添加管理员
 // 1： 用户登录名，2：昵称，3：密码（经过md5运算），4：回调，err如果成功为null，否则表示失败。属性error表示错误描述。
     db.users.addAdmin(req.body.usloginname, req.body.usnickname, req.body.usloginpsw, (err, user) => {
@@ -47,7 +47,7 @@ router.post('/addAdmin', function (req, res, next) {
         }
         else {
             res.send(user);
-            addLog(1,curuser.userid,"管理员："+curuser.nickname+"-------添加管理员："+req.body.usnickname,req);
+            addLog(1, curuser.userid, "管理员：" + curuser.nickname + "-------添加管理员：" + req.body.usnickname, req);
             console.log('======= add Admin success.');
         }
 
@@ -56,6 +56,7 @@ router.post('/addAdmin', function (req, res, next) {
 })
 
 router.post('/addCaptain', function (req, res, next) {
+    var curuser = req.body.curuser;
     // 添加团长
 // 1： 用户登录名，2：昵称，3：密码（经过md5运算），4：回调，err如果成功为null，否则表示失败。属性error表示错误描述。
     db.users.addTZ(req.body.usloginname, req.body.usnickname, req.body.usloginpsw, (err, user) => {
@@ -64,13 +65,14 @@ router.post('/addCaptain', function (req, res, next) {
             console.log(err.error);
         } else {
             res.send(user);
-            addLog(1,curuser.userid,"管理员："+curuser.nickname+"-------添加团长："+req.body.usnickname,req);
+            addLog(1, curuser.userid, "管理员：" + curuser.nickname + "-------添加团长：" + req.body.usnickname, req);
             console.log(`======= add 团长 success`);
         }
     });
 })
 
 router.post('/addMember', function (req, res, next) {
+    var curuser = req.body.curuser;
 
 // 添加团员
 // 1： 用户登录名，2：昵称，3：密码（经过md5运算），4：回调，err如果成功为null，否则表示失败。属性error表示错误描述。
@@ -80,13 +82,14 @@ router.post('/addMember', function (req, res, next) {
             console.log(err.error);
         } else {
             res.send(user);
-            addLog(1,curuser.userid,"用户："+curuser.nickname+"-------添加团员："+req.body.usnickname,req);
+            addLog(1, curuser.userid, "用户：" + curuser.nickname + "-------添加团员：" + req.body.usnickname, req);
             console.log(`======= add 团员 success`);
         }
     });
 })
 
 router.post('/updUser', function (req, res, next) {
+    var curuser = req.body.curuser;
     console.log(req.body.usloginpsw == "" ? null : req.body.usloginpsw);
 // 修改用户
 // 1：用户id，2：昵称，3：密码（为null不修改），4：回调，err如果成功为null，否则表示失败。属性error表示错误描述。
@@ -98,7 +101,7 @@ router.post('/updUser', function (req, res, next) {
         }
         else {
             res.send('success');
-            addLog(1,curuser.userid,"用户："+curuser.nickname+"-------更新用户："+req.body.usnickname,req);
+            addLog(1, curuser.userid, "用户：" + curuser.nickname + "-------更新用户：" + req.body.usnickname, req);
             console.log('======= 更新用户成功.');
         }
     });
@@ -106,31 +109,31 @@ router.post('/updUser', function (req, res, next) {
 })
 
 router.post('/delUser', function (req, res, next) {
+    var curuser = req.body.curuser;
 // 删除用户
 // 1：用户id，2：回调，err如果成功为null，否则表示失败。属性error表示错误描述。
-    db.users.delUser(req.body.usID, (err) => {
+    db.users.delUser(req.body.user.id, (err) => {
         if (err) {
             res.send("error");
             console.log(err.error);
 
         } else {
             res.send('success');
-            addLog(1,curuser.userid,"用户："+curuser.nickname+"-------删除用户："+req.body.usID,req);
+            addLog(1, curuser.userid, "用户：" + curuser.nickname + "-------删除用户：" + req.body.user.usname, req);
             console.log('======= del user success');
         }
     });
 })
-
 
 router.post('/getGames', function (req, res, next) {
 
     /*    console.log(Number(Number(req.body.start / req.body.length).toFixed(0))+1);
      console.log(Number(req.body.length));
      console.log(Number(req.body.usID));*/
-    console.log(req.body.param.usID==''?null:req.body.param.usID);
+    console.log(req.body.param.usID == '' ? null : req.body.param.usID);
 
     // 查询所有游戏 userid=0 返回所有
-    db.games.all_games(Number(Number(req.body.start / req.body.length).toFixed(0)) + 1, Number(req.body.length), req.body.param.usID==''?null:req.body.param.usID, (err, games) => {
+    db.games.all_games(Number(Number(req.body.start / req.body.length).toFixed(0)) + 1, Number(req.body.length), req.body.param.usID == '' ? null : req.body.param.usID, (err, games) => {
         if (err) {
             res.send('error');
         } else {
@@ -147,37 +150,39 @@ router.post('/getGames', function (req, res, next) {
 
 });
 
-
 router.post('/getGameWithAssigners', function (req, res, next) {
-
+    console.log(req);
+console.log(req.body);
     // 查询是谁分配的所有游戏
 // 1.pageIndex
 // 2.pageCount
 // 3.agsignerid
-    console.log(  Number(Number(req.body.start / req.body.length).toFixed(0)) + 1);
+    console.log(Number(Number(req.body.start / req.body.length).toFixed(0)) + 1);
 
     console.log(req.body.param.curuser.userid);
-db.games.all_games_with_assigner(Number(Number(req.body.start / req.body.length).toFixed(0)) + 1, Number(req.body.length), req.body.param.curuser.userid, (err, games) => {
-    if (err) {
-        res.send('error');
-    } else {
-        console.log('games');
-        console.log(games);
-        var jsonSend = {
-            data: games.games, "iTotalDisplayRecords": games.total,
-            "iTotalRecords": games.total,
-            "message": "success"
-        };
-        res.send(jsonSend);
-    }
- });
+    db.games.all_games_with_assigner(Number(Number(req.body.start / req.body.length).toFixed(0)) + 1, Number(req.body.length), req.body.param.curuser.userid, (err, games) => {
+        if (err) {
+            res.send('error');
+        } else {
+            for (var i = 0; i < games.games.length; i++) {
+                if (games.games[i].nickname == null) {
+                    games.games[i].nickname = '未分配';
+                }
+            }
+            console.log('games');
+            console.log(games);
+            var jsonSend = {
+                data: games.games, "iTotalDisplayRecords": games.total,
+                "iTotalRecords": games.total,
+                "message": "success"
+            };
+            res.send(jsonSend);
+        }
+    });
 
 });
 
-
 router.post('/addGame', function (req, res, next) {
-
-
 
     // 添加一个游戏
     // 1. userid
@@ -185,20 +190,63 @@ router.post('/addGame', function (req, res, next) {
     // 3. content
     // 4. 淘宝订单号
     // 5. 分配者用户id
-    db.games.addGame(req.body.usID, req.body.gameNickName, req.body.gameInfo, req.body.gamettddh,req.body.curuser.userid, (err, gamn) => {
+    db.games.addGame(req.body.usID, req.body.gameNickName, req.body.gameInfo, req.body.gamettddh, req.body.curuser.userid, (err, gamn) => {
         if (err) {
             res.send("error");
             console.log(err.error);
         } else {
-            gamn.nickname=req.body.nickname;
+            gamn.nickname = req.body.nickname;
             /*console.log(gamn);*/
             res.send(gamn);
-            addLog(2,req.body.curuser.userid,"管理员："+req.body.curuser.nickname+"-------添加游戏：游戏昵称为："+req.body.gameNickName,req);
+            addLog(2, req.body.curuser.userid, "管理员：" + req.body.curuser.nickname + "-------添加游戏：游戏昵称为：" + req.body.gameNickName, req);
             console.log(`======= add 游戏 success`);
         }
     });
 })
 
+router.post('/updGame', function (req, res, next) {
+    var curuser = req.body.curuser;
+
+    // 修改游戏
+// 1. id
+// 2. userid,为null或者''时，表示不更改分配的用户。否则将更改分配到的用户。
+// 3. gnickname
+// 4. content
+// 5. 淘宝订单号
+// 6. 分配者用户id，为null或者''时，表示不更改，否则将更改为新指定的分配者。
+    db.games.updateGame(req.body.gameid, req.body.usID, req.body.gameNickName, req.body.gameInfo, req.body.gamettddh, req.body.curuser.userid, (err) => {
+        if (err) {
+            res.send("error");
+            console.log(err.error);
+
+        }
+        else {
+            res.send('success');
+            addLog(2, curuser.userid, "用户：" + curuser.nickname + "-------更新游戏：" + req.body.gameNickName, req);
+            console.log('======= 更新游戏成功.');
+        }
+
+    })
+
+})
+
+router.post('/delGame', function (req, res, next) {
+    console.log(req.body.game);
+    var curuser = req.body.curuser;
+    // 删除游戏
+    // 1. id
+     db.games.delGame(req.body.game.id, (err) => {
+        if (err) {
+            res.send("error");
+            console.log(err.error);
+
+        } else {
+            res.send('success');
+            addLog(2, curuser.userid, "用户：" + curuser.nickname + "-------删除游戏：" + req.body.game.gnickname, req);
+            console.log('======= del user success');
+        }
+    });
+})
 
 router.post('/addLog', function (req, res, next) {
 
@@ -208,9 +256,9 @@ router.post('/addLog', function (req, res, next) {
 // 3. 内容
 // 4. ip?
 
-  /*  Type 1账号 2游戏 3任务 4任务列表 5 其他
-    Doer 操作者 就是userid
-    Content 操作内容（who did what）*/
+    /*  Type 1账号 2游戏 3任务 4任务列表 5 其他
+     Doer 操作者 就是userid
+     Content 操作内容（who did what）*/
     var clientipaddress = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
@@ -219,7 +267,7 @@ router.post('/addLog', function (req, res, next) {
         clientipaddress = clientipaddress.substr(7)
     }
 
-    db.logs.addlog(req.body.logtype, req.body.usID, req.body.logcontent,clientipaddress, (err) => {
+    db.logs.addlog(req.body.logtype, req.body.usID, req.body.logcontent, clientipaddress, (err) => {
         if (err) {
             console.log(err);
             return;
@@ -230,7 +278,6 @@ router.post('/addLog', function (req, res, next) {
     });
 
 
-
 })
 
 router.post('/getLogs', function (req, res, next) {
@@ -239,7 +286,7 @@ router.post('/getLogs', function (req, res, next) {
     // 1. 页号
     // 2. 每页数量
     // 3. 哪个用户的日志，0表示所有，其它表示按用户id过滤
-    db.logs.getlogs(1, 10, 0, (err, logs) => {
+    db.logs.getlogs(Number(Number(req.body.start / req.body.length).toFixed(0)) + 1, Number(req.body.length), 0, (err, logs) => {
         if (err) {
             res.send('error');
         } else {
@@ -251,12 +298,12 @@ router.post('/getLogs', function (req, res, next) {
             };
             res.send(jsonSend);
         }
-     });
+    });
 
 
 });
 
-function  addLog(logtype, usID, logcontent,req) {
+function addLog(logtype, usID, logcontent, req) {
 
     // 添加日志
 // 1. type
@@ -275,7 +322,7 @@ function  addLog(logtype, usID, logcontent,req) {
         clientipaddress = clientipaddress.substr(7)
     }
 
-    db.logs.addlog(logtype, usID, logcontent,clientipaddress, (err) => {
+    db.logs.addlog(logtype, usID, logcontent, clientipaddress, (err) => {
         if (err) {
             console.log(err);
             return;
